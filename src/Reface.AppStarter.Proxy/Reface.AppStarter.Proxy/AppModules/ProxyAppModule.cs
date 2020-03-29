1a44ac5584e1bd153c0d92d87c18d2d6abbd4fcc
ShimizuShiori <116971263@qq.com>
2020-03-29 21:40:53 +0800

    允许其它模块传递 targetModule

--- src/Reface.AppStarter.Proxy/Reface.AppStarter.Proxy/AppModules/ProxyAppModule.cs
+++ src/Reface.AppStarter.Proxy/Reface.AppStarter.Proxy/AppModules/ProxyAppModule.cs
@@ -4,10 +4,30 @@ namespace Reface.AppStarter.AppModules
 {
     public class ProxyAppModule : AppModule
     {
+        private readonly IAppModule targetModule = null;
+
+        /// <summary>
+        /// 通过构造函数指定一个 targetModule 可以更换获取代理类的 module
+        /// </summary>
+        /// <param name="targetModule"></param>
+        public ProxyAppModule(IAppModule targetModule)
+        {
+            this.targetModule = targetModule;
+        }
+        public ProxyAppModule() : this(null)
+        {
+
+        }
+
         public override void OnUsing(AppSetup setup, IAppModule targetModule)
         {
-            var infos = setup.GetScanResult(targetModule);
             ProxyAppContainerBuilder builder = setup.GetAppContainerBuilder<ProxyAppContainerBuilder>();
+            AppModuleScanResult infos;
+            if (this.targetModule != null)
+                infos = setup.GetScanResult(this.targetModule);
+            else
+                infos = setup.GetScanResult(targetModule);
+
             infos.ScannableAttributeAndTypeInfos.ForEach(x => builder.RegisterProxyOfInterface(x));
         }
     }
